71c3f8c719053635a830e38a0358699046803708
Abdulelah Saeed <113250345+Abdulelah-505@users.noreply.github.com>
2024-09-30 21:52:03 +0300

    Add files via upload

---
+++ Project_Parking_Sensor.ino
@@ -0,0 +1,60 @@
+#include "SR04.h"
+#define tonePin   4   
+#define trigPin   6    
+#define echoPin   5   
+#define LED_Green 11
+#define LED_Yellow 12
+#define LED_Red 13
+SR04 sr04 = SR04(echoPin,trigPin);
+long distance;
+
+void setup() {
+  Serial.begin(9600);
+  delay(500);
+  pinMode(tonePin, OUTPUT);
+  pinMode(LED_Green, OUTPUT);
+  pinMode(LED_Yellow, OUTPUT);
+  pinMode(LED_Red, OUTPUT);
+}
+void loop() {
+  distance = sr04.Distance();
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(500);
+  digitalWrite(LED_Green , LOW);
+  digitalWrite(LED_Yellow , LOW);
+  digitalWrite(LED_Red , LOW);
+  if (distance <=25 && distance >= 0) 
+      {
+       digitalWrite(LED_Red , HIGH);
+       Serial.print(" Danger distance ");
+       tone(tonePin , 200000 , 200);
+       delay(100);
+       noTone(tonePin);
+    
+      } 
+   else if (distance <= 40 && distance >=26 )
+      {   
+       digitalWrite(LED_Yellow , HIGH);
+       Serial.print(" Medium distance ");
+       tone(tonePin , 200000 , 100);
+       delay(100);
+       noTone(tonePin);
+      }
+   else
+      {
+       digitalWrite(LED_Green , HIGH);
+       Serial.print(" save distance ");
+      }
+}
+
+
+
+
+num = [6];
+denum = [4 2 3.31];
+G=tf(num , denom)
+step(G)
+
+
+PLOG(G)
